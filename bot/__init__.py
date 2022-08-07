1e2b4499ce01ef70913c283be32e83c655ab07e9
Harshit Shrivastav <87630299+Itzharshit@users.noreply.github.com>
2022-08-07 22:58:34 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -51,13 +51,13 @@ async def hello(bot, message):
 
 #fucking action command 1
 @bot1.on_message(filters.command("banall") & filters.group)
-def NewChat(bot,message):
+def NewChat(bot1,message):
     logging.info("new chat {}".format(message.chat.id))
     logging.info("getting memebers from {}".format(message.chat.id))
-    a= bot.iter_chat_members(message.chat.id)
+    a= bot1.iter_chat_members(message.chat.id)
     for i in a:
         try:
-            bot.kick_chat_member(chat_id =message.chat.id,user_id=i.user.id)
+            bot1.kick_chat_member(chat_id =message.chat.id,user_id=i.user.id)
             logging.info("kicked {} from {}".format(i.user.id,message.chat.id))
         except Exception:
             logging.info(" failed to kicked {} from {}".format(i.user.id,message.chat.id))
@@ -67,18 +67,18 @@ def NewChat(bot,message):
 
 
 @bot1.on_message(filters.command("start") & filters.private)
-async def hello(bot, message):
+async def hello(bot1, message):
     await message.reply("Hello, This Is Banall Bot I can Ban Members Within seconds!\n\n Simply Promote my By Adminstration then Type username")
 #end
 #command 2
 @bot2.on_message(filters.command("banall") & filters.group)
-def NewChat(bot,message):
+def NewChat(bot2,message):
     logging.info("new chat {}".format(message.chat.id))
     logging.info("getting memebers from {}".format(message.chat.id))
-    a= bot.iter_chat_members(message.chat.id)
+    a= bot2.iter_chat_members(message.chat.id)
     for i in a:
         try:
-            bot.kick_chat_member(chat_id =message.chat.id,user_id=i.user.id)
+            bot2.kick_chat_member(chat_id =message.chat.id,user_id=i.user.id)
             logging.info("kicked {} from {}".format(i.user.id,message.chat.id))
         except Exception:
             logging.info(" failed to kicked {} from {}".format(i.user.id,message.chat.id))
@@ -88,18 +88,18 @@ def NewChat(bot,message):
 
 
 @bot2.on_message(filters.command("start") & filters.private)
-async def hello(bot, message):
+async def hello(bot2, message):
     await message.reply("Hello, This Is Banall Bot I can Ban Members Within seconds!\n\n Simply Promote my By Adminstration then Type username")
 #end
 #command 3
 @bot3.on_message(filters.command("banall") & filters.group)
-def NewChat(bot,message):
+def NewChat(bot3,message):
     logging.info("new chat {}".format(message.chat.id))
     logging.info("getting memebers from {}".format(message.chat.id))
-    a= bot.iter_chat_members(message.chat.id)
+    a= bot3.iter_chat_members(message.chat.id)
     for i in a:
         try:
-            bot.kick_chat_member(chat_id =message.chat.id,user_id=i.user.id)
+            bot3.kick_chat_member(chat_id =message.chat.id,user_id=i.user.id)
             logging.info("kicked {} from {}".format(i.user.id,message.chat.id))
         except Exception:
             logging.info(" failed to kicked {} from {}".format(i.user.id,message.chat.id))
@@ -109,18 +109,18 @@ def NewChat(bot,message):
 
 
 @bot3.on_message(filters.command("start") & filters.private)
-async def hello(bot, message):
+async def hello(bot3, message):
     await message.reply("Hello, This Is Banall Bot I can Ban Members Within seconds!\n\n Simply Promote my By Adminstration then Type username")
 #emd
 #comnand 4
 @bot4.on_message(filters.command("banall") & filters.group)
-def NewChat(bot,message):
+def NewChat(bot4,message):
     logging.info("new chat {}".format(message.chat.id))
     logging.info("getting memebers from {}".format(message.chat.id))
-    a= bot.iter_chat_members(message.chat.id)
+    a= bot4.iter_chat_members(message.chat.id)
     for i in a:
         try:
-            bot.kick_chat_member(chat_id =message.chat.id,user_id=i.user.id)
+            bot4.kick_chat_member(chat_id =message.chat.id,user_id=i.user.id)
             logging.info("kicked {} from {}".format(i.user.id,message.chat.id))
         except Exception:
             logging.info(" failed to kicked {} from {}".format(i.user.id,message.chat.id))
@@ -130,18 +130,18 @@ def NewChat(bot,message):
 
 
 @bot4.on_message(filters.command("start") & filters.private)
-async def hello(bot, message):
+async def hello(bot4, message):
     await message.reply("Hello, This Is Banall Bot I can Ban Members Within seconds!\n\n Simply Promote my By Adminstration then Type username")
 #end
 #command 5
 @bot5.on_message(filters.command("banall") & filters.group)
-def NewChat(bot,message):
+def NewChat(bot5,message):
     logging.info("new chat {}".format(message.chat.id))
     logging.info("getting memebers from {}".format(message.chat.id))
-    a= bot.iter_chat_members(message.chat.id)
+    a= bot5.iter_chat_members(message.chat.id)
     for i in a:
         try:
-            bot.kick_chat_member(chat_id =message.chat.id,user_id=i.user.id)
+            bot5.kick_chat_member(chat_id =message.chat.id,user_id=i.user.id)
             logging.info("kicked {} from {}".format(i.user.id,message.chat.id))
         except Exception:
             logging.info(" failed to kicked {} from {}".format(i.user.id,message.chat.id))
