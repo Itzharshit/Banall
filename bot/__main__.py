ecab7c0e8f63d960b0fd2c67d399c2e89b35d13f
Harshit Shrivastav <87630299+Itzharshit@users.noreply.github.com>
2022-08-07 23:05:22 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,5 +1,21 @@
 from pyrogram import idle
-from . import bot
-bot.start()
+import asyncio
+from . import (bot, bot1, bot2, bot3, bot4, bot5)
+
+
+if bot:
+    bot.start()
+if bot1:
+    bot1.start()
+if bot2:
+    bot2.start()
+if bot3:
+    bot3.start()
+if bot4:
+    bot4.start()
+if bot5:
+    bot5.start()
+print("Your Spam Bots Successfully Deployed ✅")
+print("Visit -! @pyrogrammers")
+
 idle()
-bot.stop()
